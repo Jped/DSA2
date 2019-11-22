06fc6c77388a5506599a541c8c5d51281fd2853d
jped <jonathanped@gmail.com>
2019-11-22 16:21:49 -0500

    fixed up

--- project4/dynamicProgram.cpp
+++ project4/dynamicProgram.cpp
@@ -10,48 +10,69 @@ std::vector<std::vector<int>> dynamicHelperFunction(std::vector<std::vector<int>
 		// can't proceed from here.
 		return dpMatrix;
 	}
-	int string1Length = string1.length()-1;
-	int string2Length = string2.length()-1;
+	int string1Length = string1.length();
+	int string2Length = string2.length();
 	//std::cout << "STRING 1 " << string1 << " STRING 2 " << string2 << " MERGEDSTRING " << mergedString << "\n"; 
 	// check to call the dynamicHelper again
 	if (string1Length != 0 && mergedString.back() == string1.back() && dpMatrix[string1Length-1][string2Length] != 1){
 		//std::cout << " match 1 \n ";
 		dpMatrix[string1Length-1][string2Length] = 1;
-		dpMatrix = dynamicHelperFunction(dpMatrix, string1.substr(0,string1Length), string2, mergedString.substr(0, mergedString.length()-1));
+		dpMatrix = dynamicHelperFunction(dpMatrix, string1.substr(0,string1Length-1), string2, mergedString.substr(0, mergedString.length()-1));
 	}
 	if (string2Length !=0 && mergedString.back() == string2.back() && dpMatrix[string1Length][string2Length-1] != 1){
 		//std::cout << " match 2 \n";
 		dpMatrix[string1Length][string2Length-1] = 1;
-		dpMatrix = dynamicHelperFunction(dpMatrix,string1, string2.substr(0,string2Length), mergedString.substr(0, mergedString.length()-1));
+		dpMatrix = dynamicHelperFunction(dpMatrix,string1, string2.substr(0,string2Length-1), mergedString.substr(0, mergedString.length()-1));
 	}
 	return dpMatrix;
 } 
 
-std::string printSolution(std::vector<std::vector<int>> dpMatrix, std::string string1, std::string string2){}
+std::string printSolution(std::vector<std::vector<int>> dpMatrix, std::string string1, std::string string2){
+	std::string solution;
+	int i=0;
+	int j=0;
+	while(1){
+		//always perfer to go down the rows
+		if(i<=string1.length() && dpMatrix[i+1][j]){
+			solution.push_back(toupper(string1[i]));
+			i++;
+		}else if (j<=string2.length() && dpMatrix[i][j+1]){
+			solution.push_back(string2[j]);
+			j++;
+		}else{
+			break;
+		}
+	}
+	if (i < string1.length())
+		solution.push_back(toupper(string1[i]));
+	if (j < string2.length())
+		solution.push_back(string2[j]);
+	return solution;
+}
 
 
 void isInterpolation(std::string string1, std::string string2, std::string mergedString, std::string outFileName) {
 	// initialize 2d array	
 	std::vector<std::vector<int>> dpMatrix;
-	dpMatrix.resize(string1.length());
-	for (int i=0; i<string1.length(); i++)
-		dpMatrix[i].resize(string2.length());
+	dpMatrix.resize(string1.length() + 1);
+	for (int i=0; i<=string1.length(); i++)
+		dpMatrix[i].resize(string2.length()+1);
 	// call dynamicHelper function
 	dpMatrix = dynamicHelperFunction(dpMatrix, string1, string2, mergedString);
 	// check 0,0 position
 	std::ofstream outputFile(outFileName);
-	for (int i=0; i<string1.length(); i++){
+	for (int i=0; i<=string1.length(); i++){
 		std::cout << "----------------------------------\n";
 		std::cout << "| ";
-		for(int j=0; j<string2.length(); j++) {
+		for(int j=0; j<=string2.length(); j++) {
 			std::cout << dpMatrix[i][j] << " | ";
 		}
 		std::cout <<"\n";
 	}
 	if (dpMatrix[0][0] == 1)
-		outputFile << printSolution(dpMatrix, string1, string2);
+		outputFile << printSolution(dpMatrix, string1, string2) << "\n";
 	else
-		std::cout << "*** NOT A MERGE ***\n";
+		outputFile << "*** NOT A MERGE ***\n";
 	outputFile.close();
 }
 
